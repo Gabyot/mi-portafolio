eddedc825acb7dd9477dd9b7242c01eb98bb1bae
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-06-07 21:30:51 -0400

    feat: icon and href to cv

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -3,6 +3,7 @@ import githubLogoLight from '../assets/github-logo-light.svg';
 import linkedinLogoLight from '../assets/linkedin-logo-light.svg';
 import memoji from '../assets/gabyot.png'
 import './Presentation.css';
+import { FaFilePdf } from 'react-icons/fa';
 
 const Presentation = () => {
     const [text, setText] = useState('');
@@ -24,7 +25,7 @@ const Presentation = () => {
     }, []);
     return (
         <section id="presentation" className="presentation">
-            <img src={memoji} alt="memoji-me" className={`memoji ${showMemoji ? 'show' : ''}`}/>
+            <img src={memoji} alt="memoji-me" className={`memoji ${showMemoji ? 'show' : ''}`} />
             <h1 className="typing">
                 {text}
                 <span className="cursor">  </span>
@@ -32,10 +33,13 @@ const Presentation = () => {
             <p>Desarrolladora de software con experiencia en la creación de RESTful APIs en arquitecturas de microservicios.</p>
             <div className="social-links">
                 <a href="https://github.com/Gabyot" target="_blank" rel="noopener noreferrer">
-                    <img src={githubLogoLight} alt="GitHub" className="social-icon github-icon"/>
+                    <img src={githubLogoLight} alt="GitHub" className="social-icon github-icon" />
                 </a>
                 <a href="https://www.linkedin.com/in/gabriela-paz-arancibia-echeverr%C3%ADa-039a91142/" target="_blank" rel="noopener noreferrer">
-                    <img src= {linkedinLogoLight} alt="LinkedIn" className="social-icon linkedin-icon"/>
+                    <img src={linkedinLogoLight} alt="LinkedIn" className="social-icon linkedin-icon" />
+                </a>
+                <a href="https://www.canva.com/design/DAGG2Su5dfk/QHivCWj5vj0cuCpDU0OkOQ/view?utm_content=DAGG2Su5dfk&utm_campaign=designshare&utm_medium=link&utm_source=editor" target="_blank" rel="noopener noreferrer">
+                    <FaFilePdf className="social-icon cv-icon" />
                 </a>
             </div>
         </section>
